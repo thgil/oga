2e4f32f28eb01d0b1bc4a12a184b19a66a8ac589
Light <leenf@tcd.ie>
2013-01-18 09:42:40 +0000

    Added files schema

--- schema.js
+++ schema.js
@@ -1,22 +1,37 @@
 var pg = require('pg')
-  , conString = "postgres://fdwqhlmublobos:59W7Qta39KmggCqyZeZLiVza1Z@ec2-54-243-217-96.compute-1.amazonaws.com:5432/d9h6rhgaatvha8";
+  , conString = "tcp://postgres:1234@localhost/ogatest"//"postgres://fdwqhlmublobos:59W7Qta39KmggCqyZeZLiVza1Z@ec2-54-243-217-96.compute-1.amazonaws.com:5432/d9h6rhgaatvha8";
 
-  var client = new pg.Client(conString);
+var client = new pg.Client(conString)
+  , users 
+  , files;
 
 if(process.argv.length != 3) return console.log("Usage: node schema [ups/downs]");
 // Create tables code
 else if(process.argv[2] == "ups") { //, email varchar(16) not null, password not null
   client.connect();
-	query = client.query("create table users(uid serial primary key , username varchar(16) not null unique, password varchar(32) not null, email varchar(64) not null unique, joinDate timestamptz DEFAULT now(),unique(username,email))");
-	query.on('end', function() { client.end(); });
-  console.log("Tables created.")
+  client.on('drain', client.end.bind(client));
+	users = client.query("create table users(uid serial primary key"+
+                            ", username varchar(32) not null unique"+
+                            ", password varchar(32) not null"+
+                            ", email varchar(64) not null unique"+
+                            ", date timestamp DEFAULT now()"+
+                            ", unique(username,email))");
+	files = client.query("create table files(fid serial primary key"+
+                            ", uid int references users(uid)"+
+                            ", name varchar(64) not null"+
+                            ", date timestamp DEFAULT now())");
+
+  console.log("Tables users and files created.")
 }
 // Drop tables code
 else if(process.argv[2] == "downs") {
   client.connect();
-	query = client.query("drop table users");
-  query.on('end', function() { client.end(); });
-	return console.log("Tables dropped.");
+  client.on('drain', client.end.bind(client));
+  //Note order matters here files depends on users.
+  files = client.query("drop table files");
+	users = client.query("drop table users");
+
+  console.log("Tables users and files dropped."); 
 }
 
 else return console.log("Options: ups or downs");
