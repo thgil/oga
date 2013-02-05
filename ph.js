dd85a0f7a2cd18fa4eb70419403a61fe4c83ead4
Light <leenf@tcd.ie>
2013-02-05 10:41:26 +0000

    Cleaning

--- ph.js
+++
@@ -1,9 +0,0 @@
-var page = require('webpage').create();
-page.open('https://mega.co.nz/#!PBdDHDSD!YEV6GFtAKDIyD6nVzqfPHXOiYDAW2xWuRXM0bsBrlp4', function() {
-    page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js", function() {
-        page.evaluate(function() {
-            console.log("lol: ",$("body").text());
-            phantom.exit();
-        });
-    });
-});
